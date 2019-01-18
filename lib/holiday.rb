831cc6838c57b6d4c65b4506f6a152cef214f89a
Alexandra Palermo <alexmpalermo@gmail.com>
2019-01-18 19:23:54 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -59,7 +59,7 @@ def all_supplies_in_holidays(holiday_hash)
   holiday_hash.each do |season, holiday_details_hash|
   holiday_details_hash.each do |attribute, data|
     puts "#{season.capitalize}:"
-    puts "  #{attribute}: #{data}"
+    puts "  #{attribute.capitalize}: #{data.join(", ")}"
     
   # iterate through holiday_hash and print items such that your readout resembles:
   # Winter:
