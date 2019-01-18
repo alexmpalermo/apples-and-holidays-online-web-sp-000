e7da3c5ce2b6714f3d100a3c5ec84df697d46cf5
Alexandra Palermo <alexmpalermo@gmail.com>
2019-01-18 19:43:16 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -61,13 +61,6 @@ def all_supplies_in_holidays(holiday_hash)
     puts "#{season.capitalize}:"
     puts "  #{attribute.to_s.split('_').map(&:capitalize).join(' ')}: #{data.join(", ")}"
     
-    
-  # iterate through holiday_hash and print items such that your readout resembles:
-  # Winter:
-  #   Christmas: Lights, Wreath
-  #   New Years: Party Hats
-  # Summer:
-  #   Fourth Of July: Fireworks, BBQ
 end
 end
 end
